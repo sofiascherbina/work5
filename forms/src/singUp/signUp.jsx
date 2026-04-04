f193b41d5043876d29993504bdcde9e87481bde0
Софія Щербина <160267180+sofiascherbina@users.noreply.github.com>
2026-04-04 12:48:26 +0300

    done task

---
+++ forms/src/singUp/signUp.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import css from "./form.module.css";
+const INITIAL_STATE = {
+ email: "",
+ password: "",
+};
+export default function SignUpForm(){
+    const [state,setState] = useState(INITIAL_STATE);
+    const handleChange=(evt)=>{
+        const {name,value} = evt.target;
+        setState(prev=>({
+            ...prev,
+            [name]:value
+        }))
+    }
+   const handleSubmit=(evt)=>{
+        evt.preventDefault();
+        console.log(`password : ${state.password} email: ${state.email}`)
+        if(state.password.length !== 6){
+             alert('password can contain only 6 symbols');
+             return
+        }
+        if (!state.email.includes('@')){
+            alert('password must contain symbol "@"');
+            return
+        }
+        alert('Sign up is successful');
+        reset()
+    }
+    const reset = () =>{
+        setState(INITIAL_STATE);
+    }
+    return(
+        <>
+        <h1>Welcome Back</h1>
+        <form onSubmit={handleSubmit} className={css.form}>
+            <ul className={css.inputList}>
+                <li><input  name="email" value={state.email} onChange={handleChange} required placeholder="Type your email" className={css.input}/></li>
+                <li><input type="password" name="password" value={state.password} onChange={handleChange} required placeholder="Type your password" className={css.input}/></li>
+            </ul>
+            <button type="submit" className={css.submitBtn}>Sign up</button>
+        </form>
+        </>
+    )
+}
--- topic4/src/hw/TaskList.jsx
+++ topic4/src/hw/TaskList.jsx
@@ -1,32 +1,63 @@
 import { useState } from "react";
 import css from "./taskList.module.css";
 const tasksArr = [  
-    {"id": 1,"text": "Повчити конспект"},
-    {"id": 2,"text": "По розв'язувати параметри"},
-    {"id": 3,"text": "Подивитись новий фільм"},
-    {"id": 4,"text": "Повторити конспект з геометрії"},
-    {"id": 5,"text": "Попрактикувати useState в дзшці"}
+    {"id": 1,"text": "Повчити конспект", done:false},
+    {"id": 2,"text": "По розв'язувати параметри",done:false},
+    {"id": 3,"text": "Подивитись новий фільм",done:false},
+    {"id": 4,"text": "Повторити конспект з геометрії",done:false},
+    {"id": 5,"text": "Попрактикувати useState в дзшці",done:false}
 ];
+const DeleteIc=()=>{
+    return(
+        <>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 6H21" stroke="white" stroke-width="2" stroke-linecap="round"/>
+            <path d="M8 6V4H16V6" stroke="white" stroke-width="2" stroke-linecap="round"/>
+            <path d="M19 6L18 20H6L5 6" stroke="white" stroke-width="2" stroke-linecap="round"/>
+            <path d="M10 11V17" stroke="white" stroke-width="2" stroke-linecap="round"/>
+            <path d="M14 11V17" stroke="white" stroke-width="2" stroke-linecap="round"/>
+        </svg>
+        </>
+    )
+}
 export default function TaskList(){
-    const [tasks, setTask] = useState(tasksArr);
+    const [tasks, setTask] = useState(()=>{
+        let checkedArr = localStorage.getItem("tasks");
+        return checkedArr ? JSON.parse(checkedArr) : tasksArr;
+    });
     const [value, setValue] = useState("");
     function addTask(){
         let newTask = {
             id:Date.now(),
             text:value,
+            done:false
         }
-        setTask([...tasks, newTask]);
+        const newArr = [...tasks, newTask];
+        setTask(newArr);
+        localStorage.setItem("tasks", JSON.stringify(newArr))
         setValue('');
+    }
+    const changeStatus =(id)=>{
+    let completedTaks = tasks.map(task=>{
+        if(task.id === id){
+            return{ ...task, done:!task.done}
+        }
+        return task
+      })
+      setTask(completedTaks);
+      localStorage.setItem('tasks', JSON.stringify(completedTaks));
     }
     return(
         <>
         <h1 className={css.title}>Task List</h1>
         <ul className={css.taskList}>{tasks.map( task =>(<li key={task.id}>
+                    <input type="checkbox" checked={task.done} onChange={() =>changeStatus(task.id)}/>
                     <p>{task.text}</p>
                     <button onClick={()=>{
                     let filteredTasks = tasks.filter(t => t.id !== task.id);
                         setTask(filteredTasks);
-                    }} className={css.deleteBtn}>Delete task</button>
+                        localStorage.setItem("tasks", JSON.stringify(filteredTasks));
+                    }} className={css.deleteBtn}>{<DeleteIc/>}</button>
                 </li>))}</ul>
                 <div className={css.addingCont}>
                      <input type="text" value={value} onChange={txt => setValue(txt.target.value)} className={css.input} placeholder="Type ur task"/>
